c8b7e3426649aea279d86bd241f6c3ebcf1b8c78
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-18 15:44:00 +0300

    Task 8 over

--- PAST_8/Program.cs
+++ PAST_8/Program.cs
@@ -2,3 +2,11 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int startNum = 1; 
+if (num >= startNum);
+for (int i = startNum; i <= num; i++)
+if (i%2==0)
+Console.WriteLine (i + " ");

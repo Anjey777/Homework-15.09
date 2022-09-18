cec08d8ca36b19b4bc6d90ea8f2d8fb9b80aee5c
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-18 13:25:18 +0300

    Task 4 over

--- PAST_4/Program.cs
+++ PAST_4/Program.cs
@@ -3,3 +3,19 @@
 44 5 78 -> 78
 22 3 9 -> 22
 */
+
+Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int num3 = int.Parse(Console.ReadLine());
+
+int max;
+if (num1 > num2 & num1 > num3)
+max = num1;
+else if (num2 > num1 & num2 > num3)
+max = num2;
+else
+max = num3;
+Console.Write($"Наибольшее число: {max}");

ac824b84145db6f507cf10c3c72aab1dd11afa37
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-18 11:52:27 +0300

    Task 2 over

--- PAST_2/Program.cs
+++ PAST_2/Program.cs
@@ -3,3 +3,16 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+
+Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+ 
+if(num1 > num2)
+Console.WriteLine("Первое число больше второго");
+
+else if (num1 < num2)
+Console.WriteLine("Первое число меньше второго");
+else
+Console.WriteLine("Оба числа равны");
